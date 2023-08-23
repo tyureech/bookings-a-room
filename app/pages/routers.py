447007c9fde7e2f82e52c422ca99c2ef63e46b63
tyureech <tyureech@mail.ru>
2023-08-23 21:58:46 +0300

    sort imports

--- app/pages/routers.py
+++ app/pages/routers.py
@@ -1,12 +1,9 @@
 import json
-import aiofiles
-from fastapi import APIRouter, Depends, Request, File, UploadFile
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
 from app.hotels.routers import get_hotels
-
 
 router = APIRouter(prefix="/page", tags=["Html страницы"])
 
@@ -14,14 +11,12 @@ templates = Jinja2Templates("app/templates")
 
 
 @router.get("/hotels")
-async def get_page_hotels(
-    request: Request, data_hotels: json = Depends(get_hotels)
-) -> HTMLResponse:
+async def get_page_hotels(request: Request, data_hotels: json = Depends(get_hotels)):
     return templates.TemplateResponse(
         "hotels.html", context={"request": request, "hotels": data_hotels}
     )
 
 
 @router.get("")
-async def get_home(request: Request) -> HTMLResponse:
+async def get_home(request: Request):
     return templates.TemplateResponse("base.html", context={"request": request})
