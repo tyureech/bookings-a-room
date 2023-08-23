a135b73e689b22b983a3313d6033f2dd22c38b2f
tyureech <tyureech@mail.ru>
2023-08-23 21:18:47 +0300

    sort imports

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,18 +1,14 @@
 from logging.config import fileConfig
-import sys
-from os.path import abspath, dirname
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
-
-from app.database import Base
 from app.config import settings
+from app.database import Base
+from app.bookings.models import Booking
 from app.hotels.models import Hotel
 from app.hotels.rooms.models import Room
 from app.users.models import User
-from app.bookings.models import Booking
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
